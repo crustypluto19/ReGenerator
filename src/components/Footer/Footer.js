42f49fa604562ddf9f67821b148c0d547a957026
crustypluto19 <evanchristopher1907@gmail.com>
2021-07-04 01:23:23 +0200

    init footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer flex flex-col ">
+                Social Links
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
